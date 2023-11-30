956650c41db15660edd15382327910cecf5a327b
arjun-venugopal <103921812+arjun-venugopal@users.noreply.github.com>
2023-11-30 22:08:03 +0530

    Portfolio
    # My Portfolio
    
    Welcome to my GitHub repository! This is the home of my personal portfolio, where I showcase my skills, projects, and experience as a web developer and designer.
    
    ## About Me
    
    I'm [Your Name], a passionate and detail-oriented professional in the field of web development. With expertise in front-end technologies such as HTML, CSS, and JavaScript, I strive to create engaging and responsive websites.
    
    ## Projects
    
    ### Project 1: [Project Name]
    Description of Project 1. Highlight key features and technologies used.
    
    [Link to Project 1]
    
    ### Project 2: [Project Name]
    Description of Project 2. Emphasize the impact and significance of the project.
    
    [Link to Project 2]
    
    ## Skills
    
    - HTML5
    - CSS3
    - JavaScript
    - Responsive Web Design
    
    
    Thank you for visiting my portfolio!

---
+++ My Portfolio/Iam Arjun/assets/js/main.js
@@ -0,0 +1,175 @@
+/*==================== MENU SHOW Y HIDDEN ====================*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+/*==================== AUTO-TYPING ====================*/
+
+var typed = new Typed(".auto-type",{
+    strings: ["Developer", "YouTuber", "Freelancer", "Designer"],
+    typeSpeed: 100,
+    backSpeed: 150,
+    loop: true
+})
+
+/*==================== REMOVE MENU MOBILE ====================*/
+
+const navLink = document.querySelectorAll('.nav__link')
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*==================== ACCORDION SKILLS ====================*/
+
+const skillsContent = document.getElementsByClassName('skills__content'),
+      skillsHeader = document.querySelectorAll('.skills__header')
+
+function toggleSkills(){
+    let itemClass = this.parentNode.className
+
+    for(i=0; i<skillsContent.length; i++){
+        skillsContent[i].className = 'skills__content skills__close'
+    }
+    if(itemClass === 'skills__content skills__close'){
+        this.parentNode.className = 'skills__content skills__open'
+    }
+}
+skillsHeader.forEach((el) => {
+    el.addEventListener('click',toggleSkills)
+})
+
+/*==================== PORTFOLIO SWIPER  ====================*/
+let swiper = new Swiper('.swiper-container', {
+    cssMode: true,
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+function scrollHeader(){
+    const nav = document.getElementById('header')
+    if(this.scrollY >= 80) nav.classList.add('scroll-header'); 
+    else nav.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+/*==================== SHOW SCROLL UP ====================*/ 
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    if(this.scrollY >= 560) scrollUp.classList.add('show-scroll'); 
+    else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+/*==================== DARK LIGHT THEME ====================*/ 
+
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'uil-sunset'
+
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moonset' : 'uil-sunset'
+
+
+if (selectedTheme) {
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'uil-moonset' ? 'add' : 'remove'](iconTheme)
+}
+
+themeButton.addEventListener('click', () => {
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
+
+/*==================== SENT MESSAGE ====================*/ 
+
+function sendMail() {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var subject = document.getElementById("subject").value;
+    var message = document.getElementById("message").value;
+  
+
+    if (!name || !email || !subject || !message) {
+      alert("Please fill in all fields.");
+      return;
+    }
+  
+    var params = {
+      name: name,
+      email: email,
+      subject: subject,
+      message: message,
+    };
+  
+    const serviceID = "service_3mmwf4n";
+    const templateID = "template_c40mlub"; 
+  
+    emailjs.send(serviceID, templateID, params)
+      .then(function(response) {
+        console.log("Email sent successfully:", response);
+        alert("Your message has been sent successfully!");
+    
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("subject").value = "";
+        document.getElementById("message").value = "";
+      })
+      .catch(function(error) {
+        console.error("Email sending failed:", error);
+        alert("An error occurred while sending your message. Please try again later.");
+      });
+  }
+  
+
+
+   
